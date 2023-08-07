ac9f8c1dda6dabebd1cb10f1dc4b88d6f8001710
qwikk <44299295+qwakk@users.noreply.github.com>
2023-08-07 14:30:06 +0200

    implemented n amounts of iterations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,42 @@
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
-    var winner = deathRoll(1000)
-    println("Winner = Player$winner")
+    println("----------------------------")
+    println("Test iteration:")
+    var winner = deathRollWithPrintout(10000)
+    println("Winner = Player${if (winner) "1" else "2"}")
+    println("----------------------------")
+
+    val n = 1_000_000
+    val start = 10000
+    var player1Wins = 0
+    var player2Wins = 0
+
+    for (i in 0..n) {
+        if (deathRoll(start)) player1Wins++ else player2Wins++
+    }
+
+    var player1Percent:Double = (player1Wins/n.toDouble())*100
+    var player2Percent:Double = (player2Wins/n.toDouble())*100
+
+    println("n = $n")
+    println("Player1 wins = $player1Wins ($player1Percent%)")
+    println("Player2 wins = $player2Wins ($player2Percent%)")
 }
 
-fun deathRoll(start:Int):Int {
+fun deathRoll(start:Int):Boolean {
+    var limit = start
+    var isPlayer1 = true
+
+    while (limit > 1) {
+        limit = Random.nextInt(1,limit+1)
+        isPlayer1 = !isPlayer1
+    }
+
+    return isPlayer1
+}
+
+fun deathRollWithPrintout(start:Int):Boolean {
     var limit = start
     var isPlayer1 = true
 
@@ -21,5 +52,5 @@ fun deathRoll(start:Int):Int {
         isPlayer1 = !isPlayer1
     }
 
-    return if (isPlayer1) 1 else 2
+    return isPlayer1
 }
